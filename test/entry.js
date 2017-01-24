55acde895bcc1790038af0f642551e6bb05edcaa
Roman Liutikov <roman01la@romanliutikov.com>
2017-01-24 12:25:15 +0200

    update test build config & samples

--- test/entry.js
+++ test/entry.js
@@ -1,5 +1,30 @@
+var div = require('./divide');
+var R = require('ramda');
+
 require('./styles.css');
 
-var div = require('./divide');
-
 console.log(div(30, 3));
+
+var animals = [
+  {
+    name: 'goose',
+    type: 'bird',
+    color: 'white'
+  },
+  {
+    name: 'parrot',
+    type: 'bird',
+    color: 'yellow'
+  },
+  {
+    name: 'cat',
+    type: 'mammal',
+    color: 'grey'
+  }
+];
+
+var getBirds = R.filter(R.compose(R.equals('bird'), R.prop('type')));
+var getBirdsCount = R.compose(length, getBirds);
+var birdsCount = getBirdsCount(animals);
+
+console.log(birdsCount);
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var ClosureCompilerPlugin = require('../index');
 
+const concurrency = require('os').cpus().length;
+
 module.exports = [
   {
 
@@ -22,8 +24,9 @@ module.exports = [
               compiler: {
                   language_in: 'ECMASCRIPT5',
                   language_out: 'ECMASCRIPT5',
-                  compilation_level: 'SIMPLE'
+                  compilation_level: 'ADVANCED'
               },
+              concurrency
           })
       ]
   },
@@ -36,13 +39,20 @@ module.exports = [
           path: path.join(__dirname, '/'),
           filename: 'module_es6.js'
       },
+      module: {
+        loaders: [{
+          test: /\.css$/,
+          loader: 'css-loader'
+        }]
+      },
       plugins: [
           new ClosureCompilerPlugin({
               compiler: {
                   language_in: 'ECMASCRIPT6',
                   language_out: 'ECMASCRIPT5',
-                  compilation_level: 'SIMPLE'
+                  compilation_level: 'ADVANCED'
               },
+              concurrency
           })
       ]
   }
